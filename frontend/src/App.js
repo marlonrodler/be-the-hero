908ec745454ce26f551c83110b1305e79ddd7776
unknown <marlon.rodler@hotmail.com>
2020-06-22 15:25:05 -0300

    frontend do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+
+import Routes from './routes';
 
 function App() {
+  // Array [valor, funcaoDeAtualização]
+  // const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
